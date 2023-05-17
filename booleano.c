5aed16f2ea48fbca9a6a57f3ff32a2d52a9f4ae5
6511gsd <115384388+6511gsd@users.noreply.github.com>
2023-05-16 21:04:49 -0300

    Add files via upload

---
+++ booleano.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+ #include <stdio.h>
+#include <stdlib.h>
+
+
+void main()
+{
+
+
+ bool v, f;
+
+ v = true;
+ f = false;
+
+ printf("O melhor time do Brasil e o Corinthians? %d\n", v);
+ printf("2 + 2 = 5? %d\n", f);
+ system("pause");
+}
